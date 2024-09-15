2f0ae82dde9afe856ee3d86d6a62248561492a8b
mahmoud8428 <167536957+mahmoud8428@users.noreply.github.com>
2024-09-15 17:28:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body bgcolor="red">
+ <h1 style="background-color: yellow">MISR TRAVIL</h1> 
+ <table border="1">
+   <caption> this our places for our clients this week we hope to be good for you</caption>
+   <thead>
+<tr>
+   <td bgcolor="grey"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem officia odit debitis explicabo, numquam voluptatum nesciunt non nihil doloremque deleniti pariatur expedita ipsam adipisci dolorem nulla incidunt ullam, aspernatur tempora! </p></td>
+   <td> <img width="300" src="img/شاليه 2.jpg"></td>
+   <td bgcolor="grey"><p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nihil pariatur, earum, excepturi molestias placeat deserunt dignissimos sequi autem ad amet quaerat eaque quod itaque est deleniti, temporibus quis nulla.</p></td>
+   <td><img width="300" src="img/شاتليه.jpeg"></td>
+</tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td bgcolor="grey"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id tempora ea accusamus. Nostrum fugit, tempora error corrupti dolorem a nulla cum quia praesentium nam beatae, laboriosam neque, ad aliquam earum!</p></td>
+         <td> <img width="300" src="img/شاليه 3 - Copy.jpeg"></td>
+         <td bgcolor="grey"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, aperiam debitis perspiciatis odit eum quae, esse recusandae maxime provident ipsam non, officia accusantium soluta unde aut eos eius ea in.</p></td>
+         <td> <img width="300" src="img/شاليه 4.jpeg"> </td>
+      </tr>
+   </tbody>
+   <tfoot>
+      <tr>
+      <td bgcolor="grey"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi qui aliquam voluptas provident assumenda at ea, eum dolorem est perferendis facilis! Adipisci magni culpa veritatis optio officia tempora sapiente consectetur.</p></td> 
+      <td><img width="300" src="img/شاليه 3.jpeg"></td>
+      <td bgcolor="grey"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, corrupti veritatis! Officia corrupti rem quo necessitatibus, vel repellat molestias debitis provident sint veritatis magnam fugiat possimus dolorum! Aut, veniam dolore?</p></td> 
+      <td><img width="300" src="img/images 67.jpeg"></td> 
+      </tr>
+   </tfoot>
+
+ </table> 
+</body>
+</html>
